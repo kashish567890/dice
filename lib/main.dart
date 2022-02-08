209d11bace0ff294c114157668869051e6eb3a88
kashish567890 <65656675+kashish567890@users.noreply.github.com>
2022-02-08 07:10:04 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+
+
+class DicePage extends StatefulWidget {
+
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+  void diceRoll(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;//6 is not included but 0 is so if we add 1 then 6 automatically gets included and 0 gets excluded
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                diceRoll();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                diceRoll();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
